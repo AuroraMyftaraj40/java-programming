c5672455b5407e6962f2e32edbb73a09e6153d7b
AuroraMyftaraj40 <myftarajsidrit@gmail.com>
2021-05-27 19:56:39 -0400

    Address class finished

---
+++ src/day_47_constructors/Address.java
@@ -0,0 +1,54 @@
+package day_47_constructors;
+
+public class Address {
+   private String street = "9401 Lee Highway";
+   private String city = "Fairfax";
+   private String state = "VA";
+   private String zipCode = "22031";
+   private String country = "USA";
+
+    @Override
+    public String toString() {
+        return street + ", " + city + ", " + state + " " + zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
